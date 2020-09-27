9736962bf6d8337b3f5ee0c575fa799f918d471c
f <moritz.hennen@gmail.com>
2020-09-27 13:07:10 +0200

    Use ChatFormat#formatPrefix instead of buildPrefix to format messages properly

--- src/main/java/net/kitpvp/chat/Chat.java
+++ src/main/java/net/kitpvp/chat/Chat.java
@@ -38,7 +38,7 @@ public class Chat {
         text = text.replace("{%m}", format.getHighlightColor().toString()).
                 replace("{%n}", format.getNormalColor().toString());
 
-        text = "" + format.buildPrefix() + " " + format.getNormalColor() + text;
+        text = "" + format.formatPrefix() + " " + format.getNormalColor() + text;
         try {
             text = new MessageFormat(text).format(args, new StringBuffer(), new FieldPosition(0)).toString();
         } catch(Throwable t) {
