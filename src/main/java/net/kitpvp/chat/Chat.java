4baacea51495fea05f2144ff3e8fd9f6a8b304f2
f <moritz.hennen@gmail.com>
2020-11-15 23:17:06 +0100

    Fix double space offset after prefix

--- src/main/java/net/kitpvp/chat/Chat.java
+++ src/main/java/net/kitpvp/chat/Chat.java
@@ -38,7 +38,7 @@ public class Chat {
         text = text.replace("{%m}", format.getHighlightColor().toString()).
                 replace("{%n}", format.getNormalColor().toString());
 
-        text = "" + format.formatPrefix() + " " + format.getNormalColor() + text;
+        text = "" + format.formatPrefix() + text;
         try {
             text = new MessageFormat(text).format(args, new StringBuffer(), new FieldPosition(0)).toString();
         } catch(Throwable t) {
